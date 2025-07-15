d3223ca361c80d832f2e594b47ad7efae9762883
KwasiGio <giovannibuaben@gmail.com>
2025-07-15 17:32:26 +0100

    Added class "Reviews" with id, text, rating, createdAt. Setting up ManyToOne via jakarta

--- src/main/java/com/backlogbounty/backlog/model/Reviews.java
+++ src/main/java/com/backlogbounty/backlog/model/Reviews.java
@@ -1,4 +1,22 @@
 package com.backlogbounty.backlog.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Reviews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String text;
+    private int rating;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
 }
