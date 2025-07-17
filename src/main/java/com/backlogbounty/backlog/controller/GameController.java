add6a3f9c16ec1ae7d65cd7a4db235ffe7e332b4
KwasiGio <giovannibuaben@gmail.com>
2025-07-17 16:24:07 +0100

    getgamebyid is now possible and postgreSQL has been integrated

--- src/main/java/com/backlogbounty/backlog/controller/GameController.java
+++ src/main/java/com/backlogbounty/backlog/controller/GameController.java
@@ -1,8 +1,11 @@
 package com.backlogbounty.backlog.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.backlogbounty.backlog.model.Game;
 import com.backlogbounty.backlog.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,6 +19,12 @@ public class GameController {
     @GetMapping
     public List<Game> getAllGames() {
         return gameRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGameById (@PathVariable Long id) {
+        Game game = gameRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game not found"));
+        return ResponseEntity.ok(game);
     }
 
     @PostMapping
