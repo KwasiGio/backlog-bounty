3e7e1cfd1f01d4d748a294d2f306d3c7f5cbb165
KwasiGio <giovannibuaben@gmail.com>
2025-07-28 21:59:22 +0100

    Had to polish a GameProgressLog small bug

--- src/main/java/com/backlogbounty/backlog/model/GameProgressLog.java
+++ src/main/java/com/backlogbounty/backlog/model/GameProgressLog.java
@@ -1,8 +1,15 @@
 package com.backlogbounty.backlog.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class GameProgressLog {
 
     @Id
