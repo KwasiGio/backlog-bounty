d2b2c27356027a876ac77b4c3afd70913031b410
KwasiGio <giovannibuaben@gmail.com>
2025-08-14 00:16:29 +0100

    Added backlog logo, now working on navbar

--- backlog-bounty-frontend/src/components/NavBar.jsx
+++ backlog-bounty-frontend/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import logo from './Bounty.png';
 
 const NavBar = () => {
     return (
     <div className="navbar">
-        <div className="navbar-logo">Backlog Bounty</div>
-        <div className="navbar-links">
+        <div className="navbar-logo">
+            <img src ={logo} alt="Backlog Bounty Logo" className="logo-image" />
+            </div>
+            <div className ="navbar-links">
             <Link to="/">Home</Link>
             <Link to="/games">Games</Link>
             <Link to="/reviews">Reviews</Link>
