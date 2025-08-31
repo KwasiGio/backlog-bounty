3de71394b4360a07117ef4fada03848078821f38
KwasiGio <giovannibuaben@gmail.com>
2025-08-31 20:37:16 +0100

    Added PopularGames.css, added TrendingGames file but needs to be removed because of redundancy.

--- backlog-bounty-frontend/src/components/PopularGames.jsx
+++ backlog-bounty-frontend/src/components/PopularGames.jsx
@@ -1,5 +1,30 @@
 const PopularGames = () => {
-    return <nav>Navigation</nav>
+    const GamesData = [
+        {
+            id: 1,
+            title: "Ghost of Tsushima",
+            image: "https://via.placeholder.com/150"
+        },
+        {
+            id: 2,
+            title: "God of War",
+            image: "https://via.placeholder.com/150"
+        }
+    ]
+    return (
+        <div className="popular-games-container">
+            <h2>Trending Now</h2>
+            <div className= "games-grid">
+                {GamesData.map((game) => (
+                    <div key={game.id} className="game-card">
+                        <img src={game.image} alt={game.title} />
+                    <p>{game.title}</p>
+                    <span>{game.status}</span>
+                    </div>
+                    ))}
+            </div>
+        </div>
+    )
 };
 
 
